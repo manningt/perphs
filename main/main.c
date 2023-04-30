2e06fb24742e144d6c565d14f38435cb449faf6d
manningt <tom@manningetal.com>
2023-04-30 08:02:42 -0400

    remove echo_test config/comments

--- main/main.c
+++ main/main.c
@@ -1,11 +1,4 @@
-/* UART Echo Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+// main
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -15,53 +8,34 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 
-/**
- * This is an example which echos any data it receives on configured UART back to the sender,
- * with hardware flow control turned off. It does not use UART driver event queue.
- *
- * - Port: configured UART
- * - Receive (Rx) buffer: on
- * - Transmit (Tx) buffer: off
- * - Flow control: off
- * - Event queue: off
- * - Pin assignment: see defines below (See Kconfig)
- */
+#define MESSAGING_TXD_PIN 23
+#define MESSAGING_RXD_PIN 22
+#define MESSAGING_UART_PORT_NUM 2
+#define BUF_SIZE (128) //a buffer small than this causes an exception on uart_driver_install
 
-#define ECHO_TEST_TXD (CONFIG_EXAMPLE_UART_TXD)
-#define ECHO_TEST_RXD (CONFIG_EXAMPLE_UART_RXD)
-#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+#define TASK_STACK_SIZE  2048
+static const char *TAG = "LOOP";
+// static const char *STATUS = "S";
 
-#define ECHO_UART_PORT_NUM      (CONFIG_EXAMPLE_UART_PORT_NUM)
-#define ECHO_UART_BAUD_RATE     (CONFIG_EXAMPLE_UART_BAUD_RATE)
-#define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
-
-static const char *TAG = "UART TEST";
-
-#define BUF_SIZE (1024)
-
-static void echo_task(void *arg)
+static void loop_task(void *arg)
 {
     /* Configure parameters of an UART driver,
      * communication pins and install the driver */
     uart_config_t uart_config = {
         .baud_rate = 921600,
-        // .baud_rate = ECHO_UART_BAUD_RATE,
         .data_bits = UART_DATA_8_BITS,
         .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
         .source_clk = UART_SCLK_DEFAULT,
     };
-    int intr_alloc_flags = 0;
 
-#if CONFIG_UART_ISR_IN_IRAM
-    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
-#endif
-
-    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
-    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+    // the NO_CHANGE pins are for the RTS/CTS pins, which our application does not use.
+    ESP_ERROR_CHECK(uart_set_pin(MESSAGING_UART_PORT_NUM, MESSAGING_TXD_PIN, 
+        MESSAGING_RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    // BUF_SIZE*2 are the TX & RX buffers; queue size, queue handle and interrupt
+    ESP_ERROR_CHECK(uart_driver_install(MESSAGING_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(MESSAGING_UART_PORT_NUM, &uart_config));
 
     // Configure a temporary buffer for the incoming data
     uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
@@ -69,19 +43,19 @@ static void echo_task(void *arg)
     // ESP_LOGI(TAG, "portTICK_PERIOD_MS=%lu", portTICK_PERIOD_MS);
 
     while (1) {
-        // Read data from the UART
-        memset(data,0,16);
-        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, 4, 10 / portTICK_PERIOD_MS);
+        memset(data,0,8);
+        //the read_bytes(port, buffer, #bytes to read, timeout in ticks, where a tick is 1 mSec)
+        int len = uart_read_bytes(MESSAGING_UART_PORT_NUM, data, 4, 10 / portTICK_PERIOD_MS);
         if (len) {
             // data[len] = '\0';
             ESP_LOGI(TAG, "Recv str: %s data_len=%d", (char *) data, len);
         }
         // Write data back to the UART
-        uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
+        uart_write_bytes(MESSAGING_UART_PORT_NUM, (const char *) data, len);
     }
 }
 
 void app_main(void)
 {
-    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+    xTaskCreate(loop_task, "loop_task", TASK_STACK_SIZE, NULL, 10, NULL);
 }
